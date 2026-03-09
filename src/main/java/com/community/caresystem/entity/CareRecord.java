ad06cd00ece28499dcc140caff84be2009e7c278
cookie134 <1576878905@qq.com>
2026-03-09 16:32:19 +0800

    纠错

--- src/main/java/com/community/caresystem/entity/CareRecord.java
+++ src/main/java/com/community/caresystem/entity/CareRecord.java
@@ -1,6 +1,7 @@
 package com.community.caresystem.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
@@ -11,16 +12,28 @@ import java.util.Date;
 public class CareRecord {
     @TableId(type = IdType.AUTO)
     private Long id;
+
     private Long elderId;
     private String elderName;
-    private String reporterName; // 对应 reporter_name
-    private String staffName;    // 对应 staff_name
+
+    @TableField("reporter_name") // 手动指定数据库列名
+    private String reporterName;
+
+    @TableField("staff_name")
+    private String staffName;
+
     private String serviceType;
     private String serviceContent;
     private String photos;
     private String location;
     private String status;
-    private Date createTime;     // 对应 create_time
-    private Date acceptTime;     // 对应 accept_time
-    private Date finishTime;     // 对应 finish_time
+
+    @TableField("create_time")
+    private Date createTime;
+
+    @TableField("accept_time")
+    private Date acceptTime;
+
+    @TableField("finish_time")
+    private Date finishTime;
 }
